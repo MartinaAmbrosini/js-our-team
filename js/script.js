6365da26e7671a0c9cbe450b663b6deab378fe6d
Martina Ambrosini <martinaambrosini17@gmail.com>
2023-12-01 15:03:07 +0100

    feat: added team information with stamp on DOM

--- js/script.js
+++ js/script.js
@@ -5,3 +5,48 @@
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sotto forma di stringhe
+
+
+//* Creo array del team
+const myTeam = [
+    {
+      name: "Wayne Barnett",
+      role: "Founder & CEO",
+      pic: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+      name: "Angela Caroll",
+      role: "Chief Editor",
+      pic: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+      name: "Walter Gordon",
+      role: "Office Manager",
+      pic: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+      name: "Angela Lopez",
+      role: "Social Media Manager",
+      pic: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+      name: "Scott Estrada",
+      role: "Developer",
+      pic: "img/scott-estrada-developer.jpg",
+    },
+    {
+      name: "Barbara Ramos",
+      role: "Graphic Designer",
+      pic: "img/barbara-ramos-graphic-designer.jpg",
+    },
+  ];
+  console.log(myTeam);
+
+// stampo su console le informazioni di ogni membro del team
+
+for(let key in myTeam ) {
+    console.log(key, myTeam[key]);
+}
+
+// stampo le info nel dom sottoforma di stringhe
+let cardContainer = document.getElementById("card-container");
